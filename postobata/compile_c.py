a24b02f2cd111f21e11c771416bfa9d7acec6163
Rakesh Bute <rakeshbute@gmail.com>
2020-04-10 09:44:49 +0530

    Packing artefacts.

--- postobata/compile_c.py
+++ postobata/compile_c.py
@@ -2,15 +2,39 @@ import os.path
 import platform
 import re
 import subprocess
+import tarfile
 import urllib.parse as uparse
 
 import wget
+import yaml
 from pyunpack import Archive
 
 import pyposto.decorators as ppd
 
 get_url_file_name = lambda name: name[name.rfind('/') + 1:]
 extract_all = lambda archive, destination: Archive(archive).extractall(destination)
+config_default = {
+    '_os_overrides_': {
+        'Linux': {
+            'compiler_bin_url': 'https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-amd64-unknown-freebsd11.tar.xz'
+        },
+        'Darwin': {
+            'compiler_bin_url': ''
+        },
+        'Windows': {
+            'compiler_bin_url': ''
+        }
+    },
+    '_required_fields_': '',
+    'artefact_type': '',
+    'compiler_bin_url': '',
+    'libs': '',
+    'name': 'test-library',
+    'org': '',
+    'platform': 'terminal',
+    'repo': '',
+    'version': '0.0.1',
+}
 
 
 def dl_raw(url: str, folder: str = None):
@@ -47,6 +71,7 @@ def setup_home(conf):
         'root': os.path.join(user_home, '.pyposto'),
         'cache': os.path.join(user_home, '.pyposto', 'cache'),
         'raw': os.path.join(user_home, '.pyposto', 'cache', '.raw'),
+        'local': os.path.join(user_home, '.pyposto', 'cache', '.local'),
         'certs': os.path.join(user_home, '.pyposto', 'certs'),
         'venv': os.path.join(user_home, '.pyposto', 'venv'),
         'tools': os.path.join(user_home, '.pyposto', 'tools'),
@@ -57,6 +82,18 @@ def setup_home(conf):
 
 def setup_project(conf):
     project_home = os.path.abspath('.') if not '_project_root_' in conf else conf['_project_root_']
+
+    build_recipe_file = os.path.join(project_home, 'build_recipe.yml') if not '_build_recipe_' in conf else conf[
+        '_build_recipe_']
+    if os.path.exists(build_recipe_file):
+        conf['project_data'] = yaml.load(open(build_recipe_file, 'r'), yaml.Loader)
+    else:
+        raise FileNotFoundError('build_recipe.yaml doesn\'t exist')
+    build_local_override = os.path.join(project_home, 'build_override.yml') if not '_build_local_override_' in conf else \
+        conf['_build_local_override_']
+    if os.path.exists(build_local_override):
+        conf['_build_override_'] = yaml.load(open(build_local_override, 'r'), yaml.Loader)
+
     conf['project_dirs'] = {
         'root': os.path.join(project_home),
         'cache': os.path.join(project_home, '.pyposto', 'cache'),
@@ -116,29 +153,22 @@ def club_files(lib_files):
     return files
 
 
-@ppd.config({
-    '_os_overrides_': {
-        'Linux': {
-            'compiler_bin_url': 'https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-amd64-unknown-freebsd11.tar.xz'
-        },
-        'Darwin': {
-            'compiler_bin_url': ''
-        },
-        'Windows': {
-            'compiler_bin_url': ''
-        }
-    },
-    '_required_fields_': '',
-    'artefact_type': '',
-    'compiler_bin_url': '',
-    'libs': '',
-    'name': '',
-    'org': '',
-    'platform':
-        'terminal',
-    'repo': '',
-    'version': '',
-})
+def pack_artefacts(config):
+    name: str = config['name']
+    platform_name: str = config['platform']
+    version: str = config['version']
+    extension: str = 'tar'
+    build_dir: str = config['project_dirs']['build']
+    local_cache_dir: str = config['pyposto_dirs']['local']
+    artefact_name: str = f"{name}-{platform_name}-{version}.{extension}"
+    cache_path: str = os.path.join(local_cache_dir, platform_name, name, version)
+    output_filename = os.path.join(cache_path, artefact_name)
+    os.makedirs(cache_path, mode=0o770, exist_ok=True)
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(build_dir, arcname='')
+
+
+@ppd.config(config_default)
 @ppd.do(os_config_homogenizer)
 @ppd.do(setup_home)
 @ppd.do(setup_project)
@@ -148,5 +178,16 @@ def compile_c_app(conf):
     pass
 
 
+@ppd.config(config_default)
+@ppd.do(os_config_homogenizer)
+@ppd.do(setup_home)
+@ppd.do(setup_project)
+@ppd.do(setup_compiler_tool)
+@ppd.do(pack_artefacts)
+def cache_local(conf):
+    pass
+
+
 if __name__ == '__main__':
-    compile_c_app()
+    cache_local()
+    # compile_c_app()
